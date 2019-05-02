3e8419c1bb8d10440155e3d38e7c8534d830238e
chses910372 <chses910372@gmail.com>
2019-05-02 12:50:27 +0800

    Complete Ver1.0

---
+++ CO_Lab2/code/Decoder.v
@@ -0,0 +1,65 @@
+//0310020
+//Subject:     CO project 2 - Decoder
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:      Luke
+//----------------------------------------------
+//Date:        
+//----------------------------------------------
+//Description: 
+//--------------------------------------------------------------------------------
+
+module Decoder(
+    instr_op_i,
+	RegWrite_o,
+	ALU_op_o,
+	ALUSrc_o,
+	RegDst_o,
+	Branch_o
+	);
+     
+//I/O ports
+input  [6-1:0] instr_op_i;
+
+output         RegWrite_o;
+output [3-1:0] ALU_op_o;
+output         ALUSrc_o;
+output         RegDst_o;
+output         Branch_o;
+ 
+//Internal Signals
+reg    [3-1:0] ALU_op_o;
+reg            ALUSrc_o;
+reg            RegWrite_o;
+reg            RegDst_o;
+reg            Branch_o;
+
+//Parameter
+wire Rformat;
+wire lw;
+wire sw;
+wire beq;
+assign Rformat = ~(instr_op_i[0] | instr_op_i[1] | instr_op_i[2] | instr_op_i[3] | instr_op_i[4] | instr_op_i[5]);
+assign lw = instr_op_i[0] & instr_op_i[1] & ~instr_op_i[2] & ~instr_op_i[3] & ~instr_op_i[4] & instr_op_i[5];
+assign sw = instr_op_i[0] & instr_op_i[1] & ~instr_op_i[2] & instr_op_i[3] & ~instr_op_i[4] & instr_op_i[5];
+assign beq = ~instr_op_i[0] & ~instr_op_i[1] & instr_op_i[2] & ~instr_op_i[3] & ~instr_op_i[4] & ~instr_op_i[5];
+
+//Main function
+always@( instr_op_i )
+begin
+	RegDst_o 	<= Rformat;
+	ALUSrc_o 	<= lw | sw;
+	RegWrite_o 	<= Rformat | lw;
+	Branch_o	<= beq;
+	ALU_op_o 	<= {0, Rformat, beq};
+end
+
+endmodule
+
+
+
+
+
+                    
+                    
---
+++ CO_Lab2/code/Simple_Single_CPU.v
@@ -0,0 +1,142 @@
+// 0310020
+//Subject:     CO project 2 - Simple Single CPU
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:      
+//----------------------------------------------
+//Date:        
+//----------------------------------------------
+//Description: 
+//--------------------------------------------------------------------------------
+module Simple_Single_CPU(
+        clk_i,
+		rst_i
+		);
+		
+//I/O port
+input         clk_i;
+input         rst_i;
+
+//Internal Signles
+wire [31:0] constant_4 ;
+assign constant_4 = 32'b4;
+
+wire [31:0] pc_in;
+wire [31:0] pc_out;
+wire [31:0] instruction;
+wire [31:0] seq_addr;
+wire [31:0] branch_addr;
+wire [31:0] MuxWriteReg_o;
+wire [31:0] RData_1;
+wire [31:0] RData_2;
+wire [31:0] ALU_src2;
+wire [31:0] ALU_result;
+wire [31:0] extended_result;
+wire [31:0] shifted_result;
+
+// contril signal
+wire [4-1:0] ALU_ctrl;
+wire [2-1:0] ALU_op;
+wire ALU_source;
+wire branch;
+wire regwrite;
+wire regdst;
+
+wire zero;
+wire pc_source = branch & zero;
+//Greate componentes
+ProgramCounter PC(
+        .clk_i(clk_i),      
+	    .rst_i (rst_i),     
+	    .pc_in_i(pc_in) ,   
+	    .pc_out_o(pc_out) 
+	    );
+	
+Adder Adder1(
+        .src1_i(pc_out),     
+	    .src2_i(constant_4),     
+	    .sum_o(seq_addr)    
+	    );
+	
+Instr_Memory IM(
+        .pc_addr_i(pc_out),  
+	    .instr_o(instruction)    
+	    );
+
+MUX_2to1 #(.size(5)) Mux_Write_Reg(
+        .data0_i(instruction[20:16]),
+        .data1_i(instruction[15:11]),
+        .select_i(regdst),
+        .data_o(MuxWriteReg)
+        );	
+		
+Reg_File RF(
+        .clk_i(clk_i),      
+	    .rst_i(rst_i) ,     
+        .RSaddr_i(instruction[25:21]) ,  
+        .RTaddr_i(instruction[20:16]) ,  
+        .RDaddr_i(MuxWrite_o) ,  
+        .RDdata_i(ALU_result)  , 
+        .RegWrite_i (regwrite),
+        .RSdata_o(RData_1) ,  
+        .RTdata_o(RData_2)   
+        );
+	
+Decoder Decoder(
+        .instr_op_i(instruction[31:26]), 
+	    .RegWrite_o(regwrite), 
+	    .ALU_op_o(ALU_op),   
+	    .ALUSrc_o(ALU_source),   
+	    .RegDst_o(regdst),   
+		.Branch_o(branch)   
+	    );
+
+ALU_Ctrl AC(
+        .funct_i(instruction[5:0]),   
+        .ALUOp_i(ALU_op),   
+        .ALUCtrl_o(ALU_ctrl) 
+        );
+	
+Sign_Extend SE(
+        .data_i(instruction[16-1:0]),
+        .data_o(extended_result)
+        );
+
+MUX_2to1 #(.size(32)) Mux_ALUSrc(
+        .data0_i(RData_2),
+        .data1_i(extended_result),
+        .select_i(ALU_source),
+        .data_o(ALU_src2)
+        );	
+		
+ALU ALU(
+        .src1_i(RData_1),
+	    .src2_i(ALU_src2),
+	    .ctrl_i(ALU_ctrl),
+	    .result_o(ALU_result),
+		.zero_o(zero)
+	    );
+		
+Adder Adder2(
+        .src1_i(seq_addr),     
+	    .src2_i(shifted_result),     
+	    .sum_o(branch_addr)      
+	    );
+		
+Shift_Left_Two_32 Shifter(
+        .data_i(extended_result),
+        .data_o(shifted_result)
+        ); 		
+		
+MUX_2to1 #(.size(32)) Mux_PC_Source(
+        .data0_i(seq_addr),
+        .data1_i(branch_addr),
+        .select_i(pc_source),
+        .data_o(pc_in)
+        );	
+
+endmodule
+		  
+
+
