51946e09be1079f61657059f6cf4b778fbccb916
chses910372 <chses910372@gmail.com>
2019-05-14 13:56:42 +0800

    Complete Sign_Extend

--- CO_Lab3/code/Sign_Extend.v
+++ CO_Lab3/code/Sign_Extend.v
@@ -1,3 +1,4 @@
+//Student ID: 0310020
 //Subject:     CO project 2 - Sign extend
 //--------------------------------------------------------------------------------
 //Version:     1
@@ -22,6 +23,8 @@ output  [32-1:0] data_o;
 reg     [32-1:0] data_o;
 
 //Sign extended
-          
+always@(data_i)begin
+  data_o <= { { 16{data_i[15] } }, data_i };
+end
 endmodule      
      
