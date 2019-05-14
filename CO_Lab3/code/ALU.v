b12af4c43cbecaf6e9f6ae02ebfc76026fad9f8c
chses910372 <chses910372@gmail.com>
2019-05-14 13:29:04 +0800

    Complete ALU.v

--- CO_Lab3/code/ALU.v
+++ CO_Lab3/code/ALU.v
@@ -32,7 +32,16 @@ wire             zero_o;
 //Parameter
 
 //Main function
-
+assign zero_o = (result_o == 0);
+always@(ctrl_i, src1_i, src2_i) begin
+  case(ctrl_i)
+	0: result_o <= src1_i & src2_i;
+	1: result_o <= src1_i | src2_i;
+	2: result_o <= src1_i + src2_i;
+	6: result_o <= src1_i - src2_i;
+	7: result_o <= src1_i < src2_i ? 1 : 0;
+	12: result_o <= ~(src1_i | src2_i);
+  	default: result_o <= 0;
 endmodule
 
 
